f2dc2387784bba88a66c9fb706330a699e5c1d6e
Mauricio Mendoza <69234452+aodamuz@users.noreply.github.com>
2023-02-06 16:20:54 -0400

    Update main.js
    Adding basic comments to the main file.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 import Pacman from './unit/Pacman.js'
 
+// Adding the `clone` method to the Object prototype
 Object.prototype.clone = function () {
-    const newObj = (this instanceof Array) ? [] : {}
+  // Creating a new object and assigning it to the `newObj` variable
+  // If the object that the method is being called on is an instance of an Array,
+  // `newObj` will be an empty array, otherwise it will be an empty object
+  const newObj = (this instanceof Array) ? [] : {};
 
-    for (const i in this) {
-        if (i === 'clone') continue
+  // Looping through all the properties of the object that the method is being called on
+  for (const i in this) {
+    // Skipping the `clone` property to avoid infinite recursion
+    if (i === 'clone') continue;
 
-        newObj[i] = typeof this[i] === 'object' ? this[i].clone() : this[i]
-    }
+    // If the current property is an object, calling the `clone` method on it,
+    // otherwise just copying the value to the new object
+    newObj[i] = typeof this[i] === 'object' ? this[i].clone() : this[i];
+  }
 
-    return newObj
-}
+  // Returning the new object
+  return newObj;
+};
 
+// Initialize a new Pacman instance
 new Pacman(document.getElementById('pacman'))
